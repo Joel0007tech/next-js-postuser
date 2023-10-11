dfc81338253f1932fafb7c00e1184a93162f8ca1
Joel <ajiolajoel@gmail.com>
2023-10-11 15:54:19 +0100

    update to next-js postUser

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-    { id: '0', name: 'Dude Lebowski' },
-    { id: '1', name: 'Neil Young' },
-    { id: '2', name: 'Dave Gray' }
+    { id: '0', name: 'Jay Baron' },
+    { id: '1', name: 'Ayah Joan' },
+    { id: '2', name: 'Joel Ajiola' }
 ]
 
 const usersSlice = createSlice({
